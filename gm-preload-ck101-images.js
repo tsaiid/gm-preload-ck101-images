fd3a26146196743a41b8783dc377969e299c4216
I-Ta Tsai <ittsai@gmail.com>
2013-07-05 12:51:04 +0800

    Sometimes, the img will have a non-picture src.

--- gm-preload-ck101-images.js
+++ gm-preload-ck101-images.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Preload All Images in CK101
 // @namespace  http://blog.tsaiid.idv.tw/project/gmscripts/preload-all-images-in-ck101/
-// @version    0.1
+// @version    0.1.1
 // @description  The images in ck101 are not loaded initially. Use this script to load all images initially.
 // @match      http://ck101.com/*
 // @copyright  2013+, I-Ta Tsai (http://blog.tsaiid.idv.tw/)
@@ -11,7 +11,7 @@
 var $ = unsafeWindow.jQuery;
 
 $('img').each(function(){
-    if ($(this).attr('file') && !$(this).attr('src')) {
+    if ($(this).attr('file') && ($(this).attr('src') != $(this).attr('file'))) {
         $(this).attr('src', $(this).attr('file'));
     }
 });
