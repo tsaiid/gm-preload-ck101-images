533f5223313b733a35019597833a7c965221b242
I-Ta Tsai <ittsai@gmail.com>
2015-01-05 23:52:22 +0800

    use required jquery and set no conflict.

--- gm-preload-ck101-images.user.js
+++ gm-preload-ck101-images.user.js
@@ -1,17 +1,19 @@
 // ==UserScript==
-// @name       Preload All Images in CK101
-// @namespace  http://blog.tsaiid.idv.tw/project/gmscripts/preload-all-images-in-ck101/
-// @version    0.1.1
-// @description  The images in ck101 are not loaded initially. Use this script to load all images initially.
-// @match      http://ck101.com/*
-// @copyright  2013+, I-Ta Tsai (http://blog.tsaiid.idv.tw/)
+// @name          Preload All Images in CK101
+// @namespace     http://tsai.it/project/gmscripts/preload-all-images-in-ck101/
+// @version       0.1.2
+// @description   The images in ck101 are not loaded initially. Use this script to load all images initially.
+// @include       http://ck101.com/*
+// @copyright     2013+, I-Ta Tsai (http://tsai.it/)
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
+// @grant         none
 // ==/UserScript==
 
-// Use pre-loaded jQuery by ck101.
-var $ = unsafeWindow.jQuery;
+this.$ = this.jQuery = jQuery.noConflict(true);
 
 $('img').each(function(){
     if ($(this).attr('file') && ($(this).attr('src') != $(this).attr('file'))) {
         $(this).attr('src', $(this).attr('file'));
     }
 });
+
